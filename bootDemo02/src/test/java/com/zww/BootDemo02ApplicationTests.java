a4f74843dc8f4092748c7df53f5f28f1cb5b8b4a
Zww <Zww@qq.com>
2022-07-23 10:03:17 +0800

    在SpringBoot中使用Servlet

---
+++ bootDemo02/src/test/java/com/zww/BootDemo02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zww;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootDemo02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
